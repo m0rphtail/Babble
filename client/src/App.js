56ed165b5c194f2b783a0a81d3bc17c62759b052
morphtail <chitniskshitij@gmail.com>
2020-05-02 03:27:31 +0530

    setup basic client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Join from './components/Join';
 import Chat from './components/Chat';
