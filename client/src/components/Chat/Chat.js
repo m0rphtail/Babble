5346d3a7ee05424f6fd481498ed5d1ee48c0f46b
morphtail <chitniskshitij@gmail.com>
2020-05-02 04:34:27 +0530

    make chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,7 +2,24 @@ import React, {useState, useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:6969';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        console.log(socket);
+    });
+
     return (
         <h1> Chat</h1>
     )
