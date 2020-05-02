9aebe9242c8a3e934bcef775972237539e3e99f8
morphtail <chitniskshitij@gmail.com>
2020-05-02 15:57:41 +0530

    add

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -10,7 +10,7 @@ const Join = () => {
     return (
         <div className = "joinOuterContainer">
             <div className = "JoinInnerContainer">
-                <h1 className = "heading">Join</h1>
+                <h1 className = "heading">join</h1>
                 <div><input placeholder="Name" className="JoinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
                 <div><input placeholder="Room" className="JoinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
                 <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={'/chat?name='+name+'&room='+room}>
