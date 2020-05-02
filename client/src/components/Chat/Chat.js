d5166f7be8da0bf245602186b7ffaba23bab4cd0
morphtail <chitniskshitij@gmail.com>
2020-05-02 05:38:10 +0530

    done basics

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,8 +17,16 @@ const Chat = ({ location }) => {
         setName(name);
         setRoom(room);
 
-        console.log(socket);
-    });
+        socket.emit('join', { name, room }, () => {
+
+        });
+
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
+
+    }, [ENDPOINT, location.search]);
 
     return (
         <h1> Chat</h1>
