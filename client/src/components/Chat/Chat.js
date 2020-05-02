aea745b40eed2b54f3fbb6acfd88bd98e6948c3d
morphtail <chitniskshitij@gmail.com>
2020-05-02 20:46:08 +0530

    complete

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,64 +1,67 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
-import io from 'socket.io-client';
+import io from "socket.io-client";
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 import './Chat.css';
 
-import InfoBar from './../InfoBar/InfoBar';
-import Input from './../Input/Input';
-
-let socket; 
+let socket;
 
 const Chat = ({ location }) => {
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:6969';
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = 'https://m0rphtail.github.io/Babble/';
 
-    useEffect(() => {
-        const { name, room } = queryString.parse(location.search);
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
 
-        socket = io(ENDPOINT);
+    socket = io(ENDPOINT);
 
-        setName(name);
-        setRoom(room);
+    setRoom(room);
+    setName(name)
 
-        socket.emit('join', { name, room }, () => {
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+  
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
+    });
+    
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+}, []);
 
-        });
+  const sendMessage = (event) => {
+    event.preventDefault();
 
-        return () => {
-            socket.emit('disconnect');
-            socket.off();
-        }
-
-    }, [ENDPOINT, location.search]);
-
-    useEffect(() => {
-        socket.on('message', (message) => {
-            setMessages([...messages, message]);
-        })
-    }, [messages]);
-
-    const sendMessage = (event) => {
-        event.preventDefault();
-
-        if(message) {
-            socket.emit('sendMessage', message, () => setMessage(''));
-        }
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
     }
+  }
 
-    console.log(message, messages);
-
-    return (
-        <div className="outerContainer">
-            <div className="container">
-                <InfoBar room={room} />
-                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-            </div>
-        </div>
-    )
+  return (
+    <div className="outerContainer">
+      <TextContainer users={users}/>
+      <div className="container">
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
