2c48125a656bff37256b8f0655511197befc5896
morphtail <chitniskshitij@gmail.com>
2020-05-02 06:10:54 +0530

    add user management

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users');
+
 const PORT = process.env.PORT || 6969;
 
 const router = require('./router');
@@ -11,12 +13,13 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection!');
-
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room });
 
-    })
+        if(error) return callback(error);
+
+        socket.join(user.room);
+    });
 
     socket.on('disconnect', () => {
         console.log('User has left.');
