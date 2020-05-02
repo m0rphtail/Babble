1a2a069fb7e62eec3070ee4149630237ad068107
morphtail <chitniskshitij@gmail.com>
2020-05-02 16:46:55 +0530

    add chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,6 +7,8 @@ let socket;
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:6969';
 
     useEffect(() => {
@@ -28,8 +30,26 @@ const Chat = ({ location }) => {
 
     }, [ENDPOINT, location.search]);
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        })
+    }, [messages]);
+
+    const sendMessage = (event) => {
+        event.preventDefault
+
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
     return (
-        <h1> Chat</h1>
+        <div className="outerContainer">
+            <div className="Container">
+                <input value={message} onChange={(event) => setMessage(event.target.value)} onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+            </div>
+        </div>
     )
 }
 
--- server/index.js
+++ server/index.js
@@ -18,8 +18,22 @@ io.on('connection', (socket) => {
 
         if(error) return callback(error);
 
+        socket.emit('message', { user: 'admin', text: user.name + 'welcome to the room' + user.room});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: user.name + ' has joined!'});
+
         socket.join(user.room);
+
+        callback();
     });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message});
+
+        callback();
+    });
+
 
     socket.on('disconnect', () => {
         console.log('User has left.');
